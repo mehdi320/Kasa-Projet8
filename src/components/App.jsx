1d545b393e45e1a168eb477a405bc5e78c24bbf7
mehdi <fetourimehdi0@gmail.com>
2024-04-23 12:39:46 +0200

    nettoyage + premier components

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+
+import Header from "./Header"; // Assurez-vous que le chemin vers le fichier Header.jsx est correct
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+     <div className="header">
+        <h1>React App</h1>
+     </div>
+    );
+};
+
+export default Header;
