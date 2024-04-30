af9cbafbb6594243a2ee1aca4e311a9968404851
mehdi <fetourimehdi0@gmail.com>
2024-04-30 17:56:30 +0200

    modification de la structure du projet

---
+++ src/components/Thumb.jsx
@@ -0,0 +1,14 @@
+
+
+function Thumb() {
+    return (
+        <div className="thumb">
+            <div className="banner">
+            <img src="image de la maison" alt="maison" />
+            <h3 className="h3">Le titre </h3>
+            </div>
+        </div>
+    );
+}
+
+export default Thumb;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,17 @@
+import Thumb from "../Thumb";
+import Maison from "../json";
 
 
 const Home = () => {
     return (
         <div className="home">
             <div className="banner">
-            <img src="" alt="" />
+            <img src="./images/IMG.png" alt="banner" />
+            <h1 className="h1">Chez vous, partout et ailleurs</h1>
             </div>
-           
+            <div className="gallery">
+                <Thumb />
+            </div>
         </div>
     );
 };
