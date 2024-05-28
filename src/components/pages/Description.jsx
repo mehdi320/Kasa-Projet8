aebe27a9fd631d780b36e352e2f15b529f5cc7ec
mehdi <fetourimehdi0@gmail.com>
2024-05-28 14:23:39 +0200

    comitt

--- src/components/pages/Description.jsx
+++ src/components/pages/Description.jsx
@@ -1,33 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Maisons from '../../data/maison.json'; 
-import Error404 from './Error404';
-import Thumb from '../Thumb';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Maisons from './data/maison.json';
+import Error404 from './Error404'; 
 
 const Description = () => {
-  const { id } = useParams(); // Récupérez l'ID passé dans l'URL
-  console.log(id); // Vérifiez si l'ID est correctement récupéré
-
-  const maison = Maisons.find(maison => maison.id === id); // Trouvez l'élément correspondant à l'ID dans votre fichier JSON
-  console.log(maison); // Vérifiez si l'élément est correctement récupéré
-
+  const { id } = useParams();
+  const maison = Maisons.find(maison => maison.id === id);
 
   if (!maison) {
-    // Si aucun élément ne correspond à l'ID, affichez un message d'erreur
-    return (
-        <Error404 />
-    );
+    return <Error404 />;
   }
 
+  const [showText, setShowText] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowText(!showText);
+  };
+
   return (
-    <div className="description">
-      <h1>ID: {id}</h1> {/* Affiche l'ID pour vérification */}
-      {maison && (
-        <>
-          <h1>{maison.title}</h1>
-          <img src={maison.cover} alt={maison.title} />
+    <div>
+      <h1>{maison.title}</h1>
+      <Carousel
+        renderArrowPrev={(onClickHandler, hasPrev) => (
+          <button onClick={onClickHandler} disabled={!hasPrev}>
+            &#x276E;
+          </button>
+        )}
+        renderArrowNext={(onClickHandler, hasNext) => (
+          <button onClick={onClickHandler} disabled={!hasNext}>
+            &#x276F;
+          </button>
+        )}
+      >
+        {maison.pictures.map((pic, index) => (
+          <div key={index}>
+            <img src={pic} alt={`Image ${index}`} />
+          </div>
+        ))}
+      </Carousel>
+      <button onClick={handleButtonClick}>Description</button>
+      <button onClick={handleButtonClick}>Objets</button>
+      {showText && (
+        <div>
           <p>{maison.description}</p>
-        </>
+        </div>
       )}
     </div>
   );
