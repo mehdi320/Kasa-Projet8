45c4148b6535dd42ce282a920f4c59dc174e1a2e
mehdi <fetourimehdi0@gmail.com>
2024-09-27 14:27:50 +0200

    page error404 updated

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,12 +1,31 @@
-// import React from 'react';
+/**
+ * Composant Error404 - Composant d'erreur 404 avec un message et un lien vers la page d'accueil.
+ * @returns {JSX.Element} Le composant d'erreur 404.
+ */
 
-const Error404 = () => {
-    return (
-        <div>
-            <h1 className='error-404'>404</h1>
-            <p>Oups ! La page que vous demandez n&apos;existe pas.</p>
-        </div>
-    );
-};
+import { Link } from 'react-router-dom'
+import '../sass/error404.scss';
 
-export default Error404;
+function Error404() {
+  return (
+    <div className="error404">
+      <div>
+        {/* Affichage du code d'erreur 404 (404 Not Found) */}
+        <h1 className="error404-title">{`404`}</h1>
+      </div>
+      <div>
+        {/* Message d'erreur */}
+        <h2 className="error404-subtitle">{`Oups! La page que vous demandez n'existe pas.`}</h2>
+      </div>
+      <div className="error404-text-container">
+        {/* Lien vers la page d'accueil */}
+        <Link
+          to="/"
+          className="error404-text"
+        >{`Retourner sur la page d'accueil`}</Link>{' '}
+      </div>
+    </div>
+  )
+}
+
+export default Error404
